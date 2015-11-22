9f3ed77c380574847fdf8516edae2efcc38614d8
jxie007 <jxie007@e.ntu.edu.sg>
2015-11-22 18:37:51 +0800

    start working on maze, not too much code

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "maze.h"
+
+int main(void)
+{
+	while(/* not EOF*/)
+	{
+		while(/* not newline*/)
+		{
+			ch = getchar();
+			// according to mapping, translate
+			putchar();
+		}
+	}
+
+	return 0;
+}
