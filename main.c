969774d7dfc4d1105b2992f3a45a2bf3f8cbe1a4
jxie007 <jxie007@e.ntu.edu.sg>
2015-11-23 13:45:40 +0800

    finish output characters

--- main.c
+++ main.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "maze.h"
 
 int main(void)
 {
-	while(/* not EOF*/)
+	int ch;
+	FILE *fp;
+	char fname[50];
+
+	printf("Enter the name of the maze input file: ");
+	scanf("%s", fname);
+	fp = fopen(fname, "r");
+	if(fp == NULL)
 	{
-		while(/* not newline*/)
+		printf("failed to open the file. Bye.\n");
+		exit(1);
+	}
+	while((ch = getc(fp)) != EOF)
+	{
+		if(ch == '\n')
 		{
-			ch = getchar();
-			// according to mapping, translate
-			putchar();
+			putchar(ch);
+		}
+		else if(ch == 1)
+		{
+			putchar('X');
+		}
+		else if(ch == 2)
+		{
+			putchar('@');
+		}
+		else if(ch == 0)
+		{
+			putchar('.');
+		}
+		else if(ch == 3)
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar(ch);
 		}
 	}
+	putchar('\n');
+	fclose(fp);
 
 	return 0;
 }
